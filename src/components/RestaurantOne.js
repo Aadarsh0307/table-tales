9fb3c2352bf9c9bd1e17f42a93e2d33ed18e7f26
Aadarsh Rai <90710951+Aadarsh0307@users.noreply.github.com>
2023-11-22 00:17:12 +0530

    Update RestaurantOne.js

--- src/components/RestaurantOne.js
+++ src/components/RestaurantOne.js
@@ -3,6 +3,7 @@ import "./RestaurantOne.css";
 import "./BookTable.js";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import Rating from 'react-rating-stars-component';
 
 // import dayjs from 'dayjs';
 
@@ -10,10 +11,31 @@ function RestaurantOne() {
 
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [numberOfPersons, setNumberOfPersons] = useState(1);
+  const [userRating, setUserRating] = useState(0);
+  const [reviews, setReviews] = useState([]);
+  const [newReview, setNewReview] = useState('');
+  const [userAvatars, setUserAvatars] = useState({
+    userId1: 'url-to-avatar1',
+    userId2: 'url-to-avatar2',
+    // Add more user avatars as needed
+  });
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
+
+  const handleIncrement = () => {
+    if (numberOfPersons < 20) {
+    setNumberOfPersons((prevCount) => prevCount + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (numberOfPersons > 1) {
+      setNumberOfPersons((prevCount) => prevCount - 1);
+    }
+  };
+
   const handlePersonChange = (event) => {
     setNumberOfPersons(parseInt(event.target.value, 10));
   };
@@ -24,8 +46,48 @@ function RestaurantOne() {
     setSelectedTime(time);
   };
 
+  const handleRatingChange = (rating) => {
+    setUserRating(rating);
+  };
+
+  function calculateAverageRating() {
+    const totalRating = reviews.reduce((acc, review) => acc + review.rating, 0);
+    const averageRating = totalRating / reviews.length || 0;
+    return averageRating.toFixed(1);
+  }
+
+  const handleAddReview = () => {
+    if (newReview.trim() !== '') {
+      setReviews((prevReviews) => [...prevReviews, { text: newReview, rating: userRating }]);
+      setNewReview('');
+    }
+  };
+
+  const [visibleReviews, setVisibleReviews] = useState(3);
+
+  // Function to handle "Read More" click
+  const handleReadMore = () => {
+    setVisibleReviews((prevVisibleReviews) => prevVisibleReviews + 3);
+  };
+
+  // Slice the reviews array to only show the visible reviews
+  const visibleReviewsList = reviews.slice(0, visibleReviews);
+
   const Images = {
-    background: `url(${process.env.PUBLIC_URL}/restaurant-info.jpg)`,
+    background: `url(${process.env.PUBLIC_URL}/profile-background.jpg)`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+  const restaurantImg = {
+    background: `url(${process.env.PUBLIC_URL}/restaurantOne.jpg)`,
+    width: '100%', 
+    height: '100%', 
+    borderRadius: '10px',
+    objectFit: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center center',
     backgroundSize: 'cover',
@@ -54,21 +116,64 @@ function RestaurantOne() {
     <div className="full-container" style={pageStyle}>
       <div className='info-container'>
         <div className="name-container">
-          <div className="restarurant-image">
-
-          </div>
-          <div className="restaurant-name">
+          <div className="restaurant-name"  style={restaurantImg}>
             <div className="name">Name of the restaurant</div>
-            <p>Location</p>
-            <p>open form to form</p>
+            <div className="subname first">₹3000 for two | Location</div><br />
+            <div className="subname second">Open from X to Y</div>
           </div>
           <div className="about-restaurant">
-            <div className="name">
+            <div className="about">
               About
             </div>
-            <p>about the restarurant</p>
+            <div className="about-description">about the restarurant</div>
           </div>
-          <div className='name'>Images</div>
+          <div className="rating-reviews">Rating & Reviews</div>
+            <i>Average Rating: {calculateAverageRating()}</i>
+              <div className="rating-reviews-description">
+  <span>Rate the Restaurant: </span>
+  <Rating
+    value={userRating}
+    count={5}
+    onChange={handleRatingChange}
+    size={40} // Adjust the size as needed
+    activeColor="#ffd700"
+  />
+</div>
+
+<div>
+<div className="reviews-list">
+<textarea
+    rows="4"
+    cols="50"
+    value={newReview}
+    onChange={(e) => setNewReview(e.target.value)}
+    placeholder="Write a review..."
+    className="review-input"
+  />
+  <button onClick={handleAddReview} className="add-review-button">Add Review</button>
+  {visibleReviewsList.map((review, index) => (
+    <div className="review-item" key={index}>
+      {/* Display user avatar */}
+      <img
+        src={userAvatars[review.userId]}
+        style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '10px' }}
+      />
+
+      {/* Display review content */}
+      <div>
+      <span style={{marginTop: '20px'}}>Rating: {review.rating} stars</span>
+        <div className="reviews-list">{review.text}</div>
+      </div>
+    </div>
+  ))}
+</div>
+{reviews.length > visibleReviews && (
+              <button onClick={handleReadMore} className="read-more-button">
+                Read More
+              </button>
+            )}
+</div>
+          <div className='about'>Images</div>
           <div>
             <img className="rectangle2"  style={Images}></img>
             <img className="rectangle2"  style={Images}></img>
@@ -79,7 +184,7 @@ function RestaurantOne() {
           </div>
         </div>
         <div className="book-container">
-          <div className="input-group">
+          <div className="input-group1">
             <div className="date-picker-container">
               <DatePicker
                 id="dateSelector"
@@ -92,16 +197,18 @@ function RestaurantOne() {
               />
             </div>
           </div>
-          <div className="input-group text-light">
-            <label htmlFor="personSelector">Number of Persons:</label>
-            <select id="personSelector" value={numberOfPersons} onChange={handlePersonChange}>
-              {[1, 2, 3, 4, 5].map((number) => (
-                <option key={number} value={number}>
-                  {number}
-                </option>
-              ))}
-            </select>
-          </div>
+      <div className="input-group text-light">
+        <label htmlFor="personSelector">Number of Persons: </label></div>
+        <div className="input-group text-light btn-container">
+        <div className="btn-row">
+          <button onClick={handleDecrement}>-</button>
+          <span>{numberOfPersons}</span>
+          <button onClick={handleIncrement}>+</button>
+          
+        </div>
+      </div>
+      <div className="input-group text-light">
+        <label>Select your Slot: </label></div>
           <button className="rectangle">10.00 AM</button>
           <button className="rectangle">11.00 AM</button>
           <button className="rectangle">12.00 AM</button>
@@ -118,5 +225,6 @@ function RestaurantOne() {
     </div>
   )
 }
+
 
 export default RestaurantOne;
